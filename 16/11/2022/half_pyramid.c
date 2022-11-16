1e22fe9b02e1771b961efaa7c1ec64bd1e5d6c68
Bhupender Singh <96715405+bhupenderlost@users.noreply.github.com>
2022-11-16 09:35:25 +0530

    Create half_pyramid.c

---
+++ 16/11/2022/half_pyramid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j, rows;
+    printf("Enter Number Of Rows: ");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; ++i) {
+        for(j = 1; j <= i; ++j){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
